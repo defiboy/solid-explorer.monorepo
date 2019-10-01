1d330e7640e1a4b7c680929920b91cc5326fe08c
Rick Vazquez <rick_vazquez@outlook.com>
2019-10-01 06:26:03 +0100

    fix contract definition type

--- packages/solid.types/src/contract-definitions/faker.ts
+++ packages/solid.types/src/contract-definitions/faker.ts
@@ -17,7 +17,7 @@ export const buildFakeContractDefinition = ({
               return value;
           }
       }`,
-  abi = JSON.stringify(erc20ABI),
+  abi = erc20ABI,
   bytecode = erc20Bytecode,
   runtimeBycode = erc20Bytecode
 } = {}): ContractDefinition => ({
@@ -50,7 +50,7 @@ export const buildFakeContractDefinitions = (): ContractDefinition[] => {
             }
         }`,
       bytecode: erc721Bytecode,
-      abi: JSON.stringify(erc721ABI),
+      abi: erc721ABI,
       runtimeBycode: erc721Bytecode
     })
   ]
